478dd081a6ddb28d13ff671ca841222e39857ad5
devdomsos <devdomsos@gmail.com>
2020-02-19 19:37:56 +0100

    Adding Icon Component to Players

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Counter from './Counter';
+import Icon from './Icon';  
 
 class Player extends PureComponent {
   render() {
@@ -21,6 +22,7 @@ class Player extends PureComponent {
       <div className="player">
       <span className="player-name">
       <button className="remove-player" onClick={ () => removePlayer(id) }>✖</button>
+      <Icon /> 
         {name1}
       </span>
         <Counter 
